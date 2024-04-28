962369cb7e27193861d4b9cb57e781407d64047e
MNCC-Admin <mncc.dubaichurch@gmail.com>
2024-04-28 22:39:19 +0400

    New Files

---
+++ src/utils/constants.js
@@ -0,0 +1,37 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://scontent.ffjr1-5.fna.fbcdn.net/v/t39.30808-6/366798399_125533587288361_6149722882963922256_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=AifdijfsqeEAX-McIAs&_nc_ht=scontent.ffjr1-5.fna&oh=00_AfA1EO9cVcm1282iccKsnNYNAnxDIc5WGEK9qQT4GyOrEA&oe=65A47A7C';
+
+export const categories = [
+  { name: 'Home', icon: <HomeIcon />, },
+  { name: 'Vulfpeck', icon: <LiveTvIcon />, },
+  { name: 'Snarky Puppy', icon: <MusicNoteIcon />, },
+  { name: 'Jacob Collier', icon: <MusicNoteIcon />, },
+  { name: 'Dirty Loops', icon: <MusicNoteIcon />, },
+  { name: 'Adele', icon: <MusicNoteIcon />, },
+  // { <CodeIcon />, },
+  // { <MusicNoteIcon /> },
+  // { <GraphicEqIcon />, },
+  //{ <GraphicEqIcon />, },
+  // { <OndemandVideoIcon />, },
+  // { <LiveTvIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
+export const demoChannelUrl = '/channel/UC7-IG6l1wsbihL_d3AeKGWA';
+export const demoVideoUrl = '/video/GDa8kZLNhJ4';
+export const demoChannelTitle = '@MNCC-International';
+export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
